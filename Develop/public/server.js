5f198c8769cba64e7771d0d729061c38f027e71c
Luke S. <lukestroehlein18@gmail.com>
2024-04-03 17:08:55 -0400

    updating the file structure of the application

---
+++ Develop/public/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const html_routes = require('./routes/html-routes');
+const api_routes = require('./routes/api-routes');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use('/api', api-routes);
+app.use('/', html-routes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
